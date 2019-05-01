71da6f3f4c7475f549f87bb8e85e4c5a77cfbcc1
Unknown <junehopark98@gmail.com>
2019-05-01 05:09:57 -0400

    Camera Tracking with Fixed Mouse

--- js/scene.js
+++ js/scene.js
@@ -13,6 +13,7 @@ var ground;
 var player;
 var controls;
 var clock;
+var velocity = new THREE.Vector3();
 
 var MOVESPEED = 30,
     LOOKSPEED = 0.075
@@ -30,36 +31,34 @@ function init() {
 
 function createScene(){
 	// 1. set up scene
-  sceneWidth=window.innerWidth;
-  sceneHeight=window.innerHeight;
-  scene = new THREE.Scene();//the 3d scene
-  scene.fog = new THREE.FogExp2(0xf0fff0, 0.14); //enable fog
+    sceneWidth=window.innerWidth;
+    sceneHeight=window.innerHeight;
+    scene = new THREE.Scene();//the 3d scene
+    scene.fog = new THREE.FogExp2(0xf0fff0, 0.14); //enable fog
 
 	// 2. camera aka player
-  player = new Player();
-  camera = new THREE.PerspectiveCamera( 60, sceneWidth / sceneHeight, 0.1, 1000 );//perspective camera
-  camera.position.y = player.position.y;
-  camera.position.z = player.position.z;
-  
-  scene.add(camera);
+    camera = new THREE.PerspectiveCamera( 60, sceneWidth / sceneHeight, 0.1, 1000 );//perspective camera
+    camera.position.y = 2;
+    camera.position.z = 10;
+    scene.add(camera);
 
     // setup player movement
-    controls = new THREE.FirstPersonControls(camera);
-    controls.movementSpeed = MOVESPEED;
-    controls.lookSpeed= LOOKSPEED;
-    controls.lookVertical = false;
-    controls.noFly = true;
-    controls.activeLook = false;
+    controls = new THREE.PlayerControls(camera);
+    //controls.movementSpeed = MOVESPEED;
+    //controls.lookSpeed= LOOKSPEED;
+    //controls.lookVertical = false;
+    //controls.noFly = true;
+    //controls.activeLook = false;
     //document.onkeydown = handleKeyDown;
-
+    console.log(camera.position);
 	// 3. renderer
-  renderer = new THREE.WebGLRenderer({alpha:true});//renderer with transparent backdrop
+    renderer = new THREE.WebGLRenderer({alpha:true});//renderer with transparent backdrop
 	renderer.setClearColor(0xcce0ff, 1); // enable fog (??)
 	// renderer.setClearColor(scene.fog.color); // sets it to the grass color
-  renderer.shadowMap.enabled = true;//enable shadow
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-  renderer.setSize( sceneWidth, sceneHeight );
-  dom = document.getElementById('TutContainer');
+    renderer.shadowMap.enabled = true;//enable shadow
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.setSize( sceneWidth, sceneHeight );
+    dom = document.getElementById('TutContainer');
 	dom.appendChild(renderer.domElement);
 
 	// 4. lights
@@ -89,9 +88,10 @@ function createScene(){
 	hero.position.y=2;
 	scene.add( hero );
 
-	var planeGeometry = new THREE.PlaneGeometry( 5, 5, 4, 4 );
+	var planeGeometry = new THREE.PlaneGeometry( 20, 20, 4, 4 );
 	var planeMaterial = new THREE.MeshStandardMaterial( { color: 0x00ff00 } )
 
+    console.log(hero.position);
 	ground = new THREE.Mesh( planeGeometry, planeMaterial );
 	ground.receiveShadow = true;
 	ground.castShadow=false;
@@ -114,14 +114,67 @@ function update(){
     //animate
     hero.rotation.x += 0.01;
     hero.rotation.y += 0.01;
-    var delta = clock.getDelta();
-	controls.update(delta); // Move camera
+    //var delta = clock.getDelta();
+    //controls.update(delta); // Move camera
+    //playerControls();
     render();
     requestAnimationFrame(update); //request next update
+    //console.log(camera.position);
+}
+
+function playerControls() {
+    // Are the controls enabled? (Does the browser have pointer lock?)
+    if ( controls.controlsEnabled ) {
+
+        // Save the current time
+        var time = performance.now();
+        // Create a delta value based on current time
+        var delta = clock.getDelta();
+
+        // Set the velocity.x and velocity.z using the calculated time delta
+        velocity.x -= velocity.x * 10.0 * delta;
+        velocity.z -= velocity.z * 10.0 * delta;
+
+        // As velocity.y is our "gravity," calculate delta
+        velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+        if ( controls.moveForward ) {
+            velocity.z -= 400.0 * delta;
+        }
+
+        if ( controls.moveBackward ) {
+            velocity.z += 400.0 * delta;
+        }
+
+        if ( controls.moveLeft ) {
+            velocity.x -= 400.0 * delta;
+        }
+
+        if ( controls.moveRight ) {
+            velocity.x += 400.0 * delta;
+        }
+
+        // Update the position using the changed delta
+        controls.getObject().translateX( velocity.x * delta );
+        controls.getObject().translateY( velocity.y * delta );
+        controls.getObject().translateZ( velocity.z * delta );
+
+        // Prevent the camera/player from falling out of the 'world'
+        if ( controls.getObject().position.y < 10 ) {
+
+            velocity.y = 0;
+            controls.getObject().position.y = 10;
+
+        }
+
+        // Save the time for future delta calculations
+        prevTime = time;
+
+    }
 }
 
 function render(){
-  renderer.render(scene, camera);//draw
+    renderer.render(scene, camera);//draw
 }
 
 function onWindowResize() {
