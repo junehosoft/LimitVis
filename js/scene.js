25a7c3c67adf09456c0ae87e5d2be4158263226b
Jessica Pan <jzpan17@gmail.com>
2019-05-03 16:56:53 -0400

    made a sphere and gave it a glowy effect

--- js/scene.js
+++ js/scene.js
@@ -9,6 +9,7 @@ var dom;
 var hero;
 var sun;
 var key;
+var lightOrb;
 
 // room boundaries
 var ground;
@@ -175,6 +176,35 @@ function createScene(){
     scene.add(key);
 
   });
+
+  // light orbs
+  var sphereLight = new THREE.SphereGeometry(1,10,10);
+  var lightOrbMaterial = new THREE.MeshBasicMaterial(
+    { color: 0xffffff, shininess: 200 }
+  );
+
+  lightOrb = new THREE.Mesh(sphereLight, lightOrbMaterial);
+  lightOrb.position.set(4.0,4.0,4.0);
+  lightOrb.receiveShadow = false;
+  // lightOrb.castShadow = true;
+  lightOrb.scale.set(0.5,0.5,0.5);
+  scene.add(lightOrb);
+
+  // now let's make them glow? 
+  var customGlow = new THREE.ShaderMaterial({
+    uniforms: {},
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    side: THREE.BackSide,
+    blending: THREE.AdditiveBlending,
+    transparent: true
+  });
+
+  var glowSphere = new THREE.SphereGeometry(2, 20, 20);
+  var glowBall = new THREE.Mesh(glowSphere, customGlow);
+  glowBall.position.set(4.0,4.0,4.0);
+  glowBall.scale.set(0.5,0.5,0.5);
+  scene.add(glowBall);
 
   // different colors at face vertices create gradient effect
   var cubeMaterial = new THREE.MeshBasicMaterial(
