cddc6919d9b9b7cbd1a9eae744c79e14e68ce787
atgao <alice.gao@princeton.edu>
2019-05-03 08:25:38 -0400

    moved background stuff to a different folder

--- js/scene.js
+++ js/scene.js
@@ -107,56 +107,7 @@ function createScene(){
 	// hero.receiveShadow=false;
 	// hero.position.y=2;
 	// scene.add( hero );
-
-	var planeGeometry = new THREE.PlaneGeometry(1000, 1000, 4);
-	var planeMaterial = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-	ground = new THREE.Mesh( planeGeometry, planeMaterial );
-	ground.receiveShadow = true;
-	ground.castShadow = false;
-	ground.rotation.x = -Math.PI/2;
-  // console.log("ground pos")
-  // console.log(ground.position.y)
-  scene.add(ground);
-  // console.log(ground)
-
-  // set up back wall
-  var wallGeometry = new THREE.PlaneGeometry(600, 600);
-  // var wallMaterial = new THREE.MeshStandardMaterial({color: 0xdfaff7 });
-  var wallMaterial = new THREE.MeshNormalMaterial({color: 0xdfaff7});
-  wallMaterial.fog = true;
-  backWall = new THREE.Mesh(wallGeometry, wallMaterial);
-  backWall.rotation.y = Math.PI;
-  backWall.recieveShadow = true;
-  backWall.castShadow = true;
-  backWall.position.y = 50;
-  backWall.position.z = backDist;
-  scene.add(backWall);
-
-  // right wall
-  rightWall = new THREE.Mesh(wallGeometry, wallMaterial);
-  rightWall.recieveShadow = true;
-  rightWall.castShadow = true;
-  rightWall.rotation.y = -Math.PI/2;
-  rightWall.position.x = rightDist;
-  rightWall.position.y = 50;
-  scene.add(rightWall);
-
-  // left wall
-  leftWall = new THREE.Mesh(wallGeometry, wallMaterial);
-  leftWall.recieveShadow = true;
-  leftWall.castShadow = true;
-  leftWall.rotation.y = Math.PI/2;
-  leftWall.position.x = leftDist;
-  leftWall.position.y = 50;
-  scene.add(leftWall);
-
-  // front wall
-  frontWall = new THREE.Mesh(wallGeometry, wallMaterial);
-  frontWall.recieveShadow = true;
-  frontWall.castShadow = true;
-  frontWall.position.y = 50;
-  frontWall.position.z = frontDist;
-  scene.add(frontWall);
+  const sceneSubject = [new Background(scene)];
 
   // key
   let loader = new THREE.STLLoader();
