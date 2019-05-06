b0823859159d59d1e1e5da619efc794e74d19729
Jessica Pan <jzpan17@gmail.com>
2019-05-06 02:18:05 -0400

    Added a dungeon door

---
+++ js/scene/Door.js
@@ -0,0 +1,27 @@
+function Door(scene) {
+
+    let loader = new THREE.STLLoader();
+    loader.load('dungeon_door.stl', function (geometry) {
+      let material = new THREE.MeshPhongMaterial(
+        { color: 0x63e7ff, specular: 0xcefdff, shininess: 200 } );
+      door = new THREE.Mesh(geometry, material);
+  
+      door.position.set(-50,5,-200);
+      door.rotation.set(0,0,0);
+      door.scale.set(.25,.25,.25);
+  
+      door.castShadow = true;
+      door.receiveShadow = true;
+  
+      scene.add(door);
+  
+    });
+  
+  
+    this.update = function(time) {
+  
+  
+    }
+  
+  }
+  
