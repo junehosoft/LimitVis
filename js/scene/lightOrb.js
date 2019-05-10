b29468461489f1ee911106a9b53ca8ee49d00cfb
Jessica Pan <jzpan17@gmail.com>
2019-05-10 04:06:51 -0400

    pretty

--- js/scene/lightOrb.js
+++ js/scene/lightOrb.js
@@ -43,36 +43,6 @@
     glowBox.scale.set(0.35,0.35,0.35);
     glows.push(glowBox);
     scene.add(glowBox);
-  
-
-  // // light orbs
-  // var sphereLight = new THREE.SphereGeometry(1,10,10);
-  // var lightOrbMaterial = new THREE.MeshBasicMaterial(
-  //   { color: 0xffffff, shininess: 200 }
-  // );
-
-  // lightOrb = new THREE.Mesh(sphereLight, lightOrbMaterial);
-  // lightOrb.position.set(2.0,2.0,2.0);
-  // lightOrb.receiveShadow = false;
-  // // lightOrb.castShadow = true;
-  // lightOrb.scale.set(0.5,0.5,0.5);
-  // scene.add(lightOrb);
-
-  // // now let's make them glow?
-  // var customGlow = new THREE.ShaderMaterial({
-  //   uniforms: {},
-  //   vertexShader: document.getElementById('vertexShader').textContent,
-  //   fragmentShader: document.getElementById('fragmentShader').textContent,
-  //   side: THREE.BackSide,
-  //   blending: THREE.AdditiveBlending,
-  //   transparent: true
-  // });
-
-  // var glowSphere = new THREE.SphereGeometry(2, 20, 20);
-  // var glowBall = new THREE.Mesh(glowSphere, customGlow);
-  // glowBall.position.set(2.0,2.0,2.0);
-  // glowBall.scale.set(0.5,0.5,0.5);
-  // scene.add(glowBall);
 
   this.update = function (time) {
 
